dd3c16740175c81830cc209f7fe6c043f96f21a2
David Tang <dtang859@gmail.com>
2022-01-30 23:54:59 -0800

    4.6 - Editing an album

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -14,6 +14,7 @@ class AlbumController extends Controller
             ->orderBy('artist')
             ->orderBy('title')
             ->get([
+                'albums.id',
                 'albums.title',
                 'artists.name AS artist',
             ]);
@@ -51,5 +52,37 @@ class AlbumController extends Controller
         return redirect()
             ->route('albums.index')
             ->with('success', "Successfully created {$artist->name} - {$request->input('title')}");
+    }
+
+    public function edit($id)
+    {
+        $album = DB::table('albums')->where('id', '=', $id)->first();
+        $artists = DB::table('artists')->orderBy('name')->get();
+
+        return view('album.edit', [
+            'album' => $album,
+            'artists' => $artists,
+        ]);
+    }
+
+    public function update($id, Request $request)
+    {
+        $request->validate([
+            'title' => 'required|max:20',
+            'artist' => 'required|exists:artists,id',
+        ]);
+
+        DB::table('albums')->where('id', '=', $id)->update([
+            'title' => $request->input('title'),
+            'artist_id' => $request->input('artist'),
+        ]);
+
+        $artist = DB::table('artists')
+            ->where('id', '=', $request->input('artist'))
+            ->first();
+
+        return redirect()
+            ->route('album.edit', [ 'id' => $id ])
+            ->with('success', "Successfully updated {$artist->name} - {$request->input('title')}");
     }
 }
