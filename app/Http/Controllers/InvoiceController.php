986ad759beb9189553fe30ab9aa394470c65417d
David Tang <dtang859@gmail.com>
2022-03-19 15:17:52 -0700

    Week 11.1 - Authorizing access to /invoices/{id} with a Gate

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -5,12 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Invoice;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class InvoiceController extends Controller
 {
     public function index()
     {
-        $invoices = Invoice::with(['customer'])->get();
+        $invoices = Invoice::select('invoices.*')
+            ->with(['customer'])
+            ->join('customers', 'invoices.customer_id', '=', 'customers.id')
+            ->where('customers.email', '=', Auth::user()->email)
+            ->get();
         
         return view('invoice.index', [
             'invoices' => $invoices
@@ -24,6 +30,24 @@ class InvoiceController extends Controller
             'invoiceItems.track.album',
             'invoiceItems.track.album.artist',
         ])->find($id);
+
+        if (Gate::denies('view-invoice', $invoice)) {
+            abort(403);
+        }
+
+        // if (!Gate::allows('view-invoice', $invoice)) {
+        //     abort(403);
+        // }
+
+        // if (Auth::user()->cannot('view-invoice', $invoice)) {
+        //     abort(403);
+        // }
+
+        // if (!Auth::user()->can('view-invoice', $invoice)) {
+        //     abort(403);
+        // }
+
+        // $this->authorize('view-invoice', $invoice);
 
         return view('invoice.show', [
             'invoice' => $invoice,
