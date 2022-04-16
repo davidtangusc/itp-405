7e540f848e2556dd0c911c017b42a37162426014
David Tang <dtang859@gmail.com>
2022-04-16 09:48:28 -0700

    15.3 - A real-time album search

--- resources/views/livewire/search-albums.blade.php
+++ resources/views/livewire/search-albums.blade.php
@@ -1,25 +1,29 @@
-<table class="table table-striped">
-    <thead>
-        <tr>
-            <th>Album</th>
-            <th colspan="2">Artist</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($albums as $album)
+<div>
+    <input wire:model="search" type="text" placeholder="Search albums..." />
+
+    <table class="table table-striped">
+        <thead>
             <tr>
-                <td>
-                    {{$album->title}}
-                </td>
-                <td>
-                    {{$album->artist}}
-                </td>
-                <td>
-                    <a href="{{ route('album.edit', [ 'id' => $album->id ]) }}">
-                        Edit
-                    </a>
-                </td>
+                <th>Album</th>
+                <th colspan="2">Artist</th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            @foreach($albums as $album)
+                <tr>
+                    <td>
+                        {{$album->title}}
+                    </td>
+                    <td>
+                        {{$album->artist}}
+                    </td>
+                    <td>
+                        <a href="{{ route('album.edit', [ 'id' => $album->id ]) }}">
+                            Edit
+                        </a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
